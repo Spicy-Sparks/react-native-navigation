aa76f4f4da0ece8f68d6071c44640cfe96fc302a
Alessandro Gerelli <geroale2000@gmail.com>
2023-06-30 10:50:26 +0200

    Update MotionEvent.kt

--- lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
@@ -4,24 +4,36 @@ import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import com.reactnativenavigation.BuildConfig
 
 val hitRect = Rect()
 
 fun MotionEvent.coordinatesInsideView(view: View?): Boolean {
-    if (BuildConfig.REACT_NATVE_VERSION_MINOR < 71) {
+
+    var hit = false
+
+    /*if (!hit) {
         view ?: return false
         view.getHitRect(hitRect)
-        return hitRect.contains(x.toInt(), y.toInt())
-    } else {
-        val viewGroup = (view as? ViewGroup) ?: return false // ?.getChildAt(0) as? ViewGroup ?: return false
+        hit = hitRect.contains(x.toInt(), y.toInt())
+    }*/
 
-        return if (viewGroup.childCount > 0) {
+    if (!hit) {
+        val viewGroup = (view as? ViewGroup) ?: return false
+        if (viewGroup.childCount > 0) {
             val content = viewGroup.getChildAt(0)
             content.getHitRect(hitRect)
-            hitRect.contains(x.toInt(), y.toInt())
-        } else {
-            false
+            hit = hitRect.contains(x.toInt(), y.toInt())
         }
     }
+
+    if (!hit) {
+        val viewGroup = (view as? ViewGroup)?.getChildAt(0) as? ViewGroup ?: return false
+        if (viewGroup.childCount > 0) {
+            val content = viewGroup.getChildAt(0)
+            content.getHitRect(hitRect)
+            hit = hitRect.contains(x.toInt(), y.toInt())
+        }
+    }
+
+    return hit
 }
