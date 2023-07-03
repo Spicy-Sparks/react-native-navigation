0515b965f1c1058b1fb15ea796b94276fe9cae00
Alessandro Gerelli <geroale2000@gmail.com>
2023-07-03 10:34:05 +0200

    Update MotionEvent.kt

--- lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
@@ -4,13 +4,14 @@ import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import com.reactnativenavigation.BuildConfig
 
 val hitRect = Rect()
 
 fun MotionEvent.coordinatesInsideView(view: View?): Boolean {
     var hit = false
 
-    if (!hit) {
+    if (!hit && BuildConfig.DEBUG) {
         view ?: return false
         view.getHitRect(hitRect)
         hit = hitRect.contains(x.toInt(), y.toInt())
